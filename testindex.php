69d6ad5ff014e369d1ad328e436106bd19dd5c93
Nathan Chavez <nathanchavezpromail@gmail.com>
2024-04-03 22:08:40 -0500

    Update testindex.php

--- testindex.php
+++ testindex.php
@@ -73,9 +73,9 @@
   <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="hero-container" data-aos="fade-in">
       <h1>Nathan Chavez</h1>
-      <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+      <p>I'm a <span class="typed" data-typed-items="Student, Developer, Gamer"></span></p>
     </div>
-  </section><!-- End Hero -->
+  	</section><!-- End Hero -->
 
   <main id="main">
 
