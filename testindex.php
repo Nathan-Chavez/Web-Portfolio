548ada0dd51c112dfd9036aa7281e7b6e9ff27f0
Nathan Chavez <nathanchavezpromail@gmail.com>
2024-04-03 23:29:36 -0500

    Update testindex.php

--- testindex.php
+++ testindex.php
@@ -45,9 +45,13 @@
     <div class="d-flex flex-column">
 
       <div class="profile">
-        <!-- <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle"> 
-        <h1 class="text-light"><a href="index.html">Nathan Chavez</a></h1> --->
+        <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Nathan Chavez</a></h1>
         <div class="social-links mt-3 text-center">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
           <a href="https://www.linkedin.com/in/nathan-e-chavez/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
         </div>
       </div>
@@ -69,9 +73,9 @@
   <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="hero-container" data-aos="fade-in">
       <h1>Nathan Chavez</h1>
-      <p>I'm a <span class="typed" data-typed-items="Student, Developer, Gamer"></span></p>
+      <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
     </div>
-  	</section><!-- End Hero -->
+  </section><!-- End Hero -->
 
   <main id="main">
 
@@ -529,6 +533,7 @@
 
       </div>
     </section><!-- End Contact Section -->
+
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
